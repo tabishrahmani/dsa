552c5546911971262afc283578f8c5c88169e2f7
tabishrahmani <tabishrahmani@hotmail.com>
2026-03-17 00:02:55 +0530

    set matrix zero brute

---
+++ 1_arrays/1.1_set_matrix_zero/brute.ts
@@ -0,0 +1,27 @@
+function setZeroes(matrix: number[][]): void {
+  const zeros: number[][] = [];
+  for (const row in matrix) {
+    for (const col in matrix[row]) {
+      if (!matrix[row][col]) zeros.push([+row, +col]);
+    }
+  }
+
+  if (zeros.length) {
+    for (const [row, col] of zeros) {
+      for (let i = 0; i < matrix.length; i++) {
+        matrix[i][col] = 0;
+      }
+      for (let i = 0; i < matrix[0].length; i++) {
+        matrix[row][i] = 0;
+      }
+    }
+  }
+}
+
+const matrix = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+];
+
+setZeroes(matrix);
